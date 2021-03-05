ee19c1ec5935f21dbfc8a2efdb8aed4494f9e396
christian <kalmecak@hotmail.com>
2021-03-05 13:32:56 -0500

    Update URLSign.go

--- URLSign.go
+++ URLSign.go
@@ -7,7 +7,7 @@ import (
 )
 
 // URLSign genera una firma en string con formato Base64 apta para el envío en métodos http, se usa
-// la infromación que se la pasa utilizando SHA-256
+// la información que se la pasa utilizando SHA-256
 func URLSign(data, key []byte) string {
 
 	mac := hmac.New(sha256.New, key)
